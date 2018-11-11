dc3249e53a1dde1af22c65b8f135bdeb1be80000
Henry Chan <Henry Chan>
2018-11-11 10:15:55 -0800

    new files

---
+++ src/main/java/ca/mec/productsbyweatherforecast/MecProductsByWeatherForecastApplication.java
@@ -0,0 +1,12 @@
+package ca.mec.productsbyweatherforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecProductsByWeatherForecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecProductsByWeatherForecastApplication.class, args);
+	}
+}
